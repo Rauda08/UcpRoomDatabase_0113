302fd28d418fbb5839141a395441a155876c6947
Rauda08 <raudajnnh08@gmail.com>
2024-12-16 17:18:25 +0700

    menambahkan pada fungsi insertdosen

--- app/src/main/java/com/example/ucp2/data/dao/DosenDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/DosenDao.kt
@@ -1,4 +1,11 @@
 package com.example.ucp2.data.dao
 
+import androidx.room.Insert
+import com.example.ucp2.data.entity.Dosen
+
 interface DosenDao {
+    @Insert
+    suspend fun insertDosen(
+        dosen: Dosen
+    )
 }
