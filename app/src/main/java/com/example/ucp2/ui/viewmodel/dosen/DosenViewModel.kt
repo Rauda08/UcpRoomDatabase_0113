b0c4c6b41490e43c268bc3b5db0f17ff4dc1b441
Rauda08 <raudajnnh08@gmail.com>
2024-12-20 00:20:01 +0700

    menambah dataclass

--- app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DosenViewModel.kt
@@ -1,8 +1,56 @@
 package com.example.ucp2.ui.viewmodel.dosen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.ucp2.data.entity.Dosen
 import com.example.ucp2.repository.RepositoryDosen
 
 class DosenViewModel(private val repositoryDosen: RepositoryDosen) : ViewModel() {
 
+    var uiState by mutableStateOf(DosenUIState())
 
+    fun updateState(dosenEvent: DosenEvent) {
+        uiState = uiState.copy(
+            dosenEvent = dosenEvent,
+        )
+    }
+
+
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(
+            snackBarMessage = null
+        )
+    }
+}
+
+data class DosenUIState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
+    val isLoading: Boolean = false,
+    val dosenList: List<Dosen> = emptyList()
+)
+
+data class FormErrorState(
+    val nidn: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null
+) {
+    fun isValid(): Boolean {
+        return nidn == null && nama == null && jenisKelamin == null
+    }
+}
+
+fun DosenEvent.toDosenEntity(): Dosen = Dosen(
+    nidn = nidn,
+    nama = nama,
+    jenisKelamin = jenisKelamin
+)
+
+data class DosenEvent(
+    val nidn: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = ""
+)
