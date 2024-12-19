1db4fe60fdb9b4eaf7eec5f1935f12dec6bec457
Rauda08 <raudajnnh08@gmail.com>
2024-12-19 19:37:16 +0700

    membuat operasi insert, delete, update

--- app/src/main/java/com/example/ucp2/data/dao/MataKuliahDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/MataKuliahDao.kt
@@ -1,8 +1,12 @@
 package com.example.ucp2.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
@@ -11,6 +15,17 @@ interface MataKuliahDao {
     suspend fun insertMataKuliah(
         mataKuliah: MataKuliah
     )
+    @Query("SELECT * FROM matakuliah ORDER BY nama ASC")
+    fun getAllMataKuliah () : Flow<List<MataKuliah>>
+
+    @Query("SELECT * FROM matakuliah WHERE kode = :kode")
+    fun getMataKuliah (kode: String) : Flow<MataKuliah>
+
+    @Delete
+    suspend fun deleteMataKuliah (mataKuliah: MataKuliah)
+
+    @Update
+    suspend fun updateMataKuliah (mataKuliah: MataKuliah)
 
 
 }
