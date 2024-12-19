cc1bc2fc1ba66a76c972fac9f5a338d78a56c195
Rauda08 <raudajnnh08@gmail.com>
2024-12-19 19:57:32 +0700

    menambah implementasi untuk roomdatabase

--- app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
+++ app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
@@ -1,11 +1,31 @@
 package com.example.ucp2.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.dao.MataKuliahDao
 import com.example.ucp2.data.entity.Dosen
 
 @Database(entities = [Dosen::class], version = 1, exportSchema = false)
 abstract class KrsDatabase : RoomDatabase(){
     abstract fun dosenDao(): DosenDao
+    abstract fun mataKuliahDao(): MataKuliahDao
+
+    companion object {
+        @Volatile
+        private var Instance: KrsDatabase? = null
+
+        fun getDatabase(context: Context): KrsDatabase {
+            return (Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    KrsDatabase::class.java,
+                    "KrsDatabase"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
 }
