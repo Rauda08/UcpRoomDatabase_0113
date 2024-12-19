081205c61079143e9c732b6d4c3cd100932cd88d
Rauda08 <raudajnnh08@gmail.com>
2024-12-20 00:20:55 +0700

    funcition validasi dan save data

--- app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DosenViewModel.kt
@@ -4,8 +4,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Dosen
 import com.example.ucp2.repository.RepositoryDosen
+import kotlinx.coroutines.launch
 
 class DosenViewModel(private val repositoryDosen: RepositoryDosen) : ViewModel() {
 
@@ -17,6 +19,40 @@ class DosenViewModel(private val repositoryDosen: RepositoryDosen) : ViewModel()
         )
     }
 
+    private fun validateFields(): Boolean {
+        val event = uiState.dosenEvent
+        val errorState = FormErrorState(
+            nidn = if (event.nidn.isNotEmpty()) null else "NIDN tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun saveData() {
+        val currentEvent = uiState.dosenEvent
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryDosen.insertDosen(currentEvent.toDosenEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data Berhasil Disimpan",
+                        dosenEvent = DosenEvent(), // reset input form
+                        isEntryValid = FormErrorState() // reset error state
+                    )
+                } catch (e: Exception) {
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data Gagal Disimpan"
+                    )
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Data tidak valid. Periksa kembali data anda"
+            )
+        }
+    }
 
     fun resetSnackBarMessage() {
         uiState = uiState.copy(
