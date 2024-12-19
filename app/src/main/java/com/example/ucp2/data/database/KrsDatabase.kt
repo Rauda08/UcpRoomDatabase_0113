bce752943054ea7d197de6ed716449593a7c34d9
Rauda08 <raudajnnh08@gmail.com>
2024-12-19 18:57:17 +0700

    Inisialisasi database Room untuk Dosen

--- app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
+++ app/src/main/java/com/example/ucp2/data/database/KrsDatabase.kt
@@ -1,3 +1,12 @@
 package com.example.ucp2.data.database
 
-data class KrsDatabase()
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.entity.Dosen
+
+@Database(entities = [Dosen::class], version = 1, exportSchema = false)
+abstract class KrsDatabase : RoomDatabase(){
+    abstract fun dosenDao(): DosenDao
+
+}
