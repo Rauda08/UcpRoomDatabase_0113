b1ab6fe24adb75e614c1b66e5894df3f1ade97b8
Rauda08 <raudajnnh08@gmail.com>
2024-12-19 20:36:22 +0700

    memperbarui operasi pada tabel DosenDao

--- app/src/main/java/com/example/ucp2/data/dao/DosenDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/DosenDao.kt
@@ -1,9 +1,7 @@
 package com.example.ucp2.data.dao
 
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
 import com.example.ucp2.data.entity.Dosen
 import kotlinx.coroutines.flow.Flow
 
@@ -17,10 +15,4 @@ interface DosenDao {
 
     @Query ("SELECT * FROM dosen WHERE nidn = :nidn")
     fun getDosen (nidn: String) : Flow<Dosen>
-
-    @Delete
-    suspend fun deleteDosen (dosen: Dosen)
-
-    @Update
-    suspend fun updateDosen (dosen: Dosen)
 }
