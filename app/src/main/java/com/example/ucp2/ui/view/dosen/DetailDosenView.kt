a1758a0bd0c1e301a7180f3abe3cd35913559d6f
Rauda08 <raudajnnh08@gmail.com>
2024-12-20 00:27:09 +0700

    percobaan membuat DetailDosenView

--- app/src/main/java/com/example/ucp2/ui/view/dosen/DetailDosenView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/DetailDosenView.kt
@@ -1,8 +1,33 @@
 package com.example.ucp2.ui.view.dosen
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.viewmodel.dosen.DosenViewModel
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 
 @Composable
-fun InsertDosenView(
+fun DetailDosenView (
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DosenViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState //ambil ui state dari viewmodel
+    val snackbarHostState = remember { SnackbarHostState() } //snackbar state
+    val coroutineScope = rememberCoroutineScope()
 
-)
+    //observasi perubahan snackbarmessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message) //tampilan snackbar
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+}
