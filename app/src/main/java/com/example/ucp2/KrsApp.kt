7f15e8bfc7b7b0f0c6bc9caf1a69cf7cc06af4e9
Rauda08 <raudajnnh08@gmail.com>
2024-12-20 19:44:13 +0700

    membuat class KrsApp

--- app/src/main/java/com/example/ucp2/KrsApp.kt
+++ app/src/main/java/com/example/ucp2/KrsApp.kt
@@ -1,4 +1,13 @@
 package com.example.ucp2
 
-class KrsApp {
+import android.app.Application
+import com.example.ucp2.dependenciesinjection.ContainerApp
+
+class KrsApp : Application() {
+    lateinit var containerApp: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        containerApp = ContainerApp(this)
+    }
 }
