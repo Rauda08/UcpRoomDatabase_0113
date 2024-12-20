48d5ede2cba2571c596d927131306821c7f06962
Rauda08 <raudajnnh08@gmail.com>
2024-12-20 21:23:17 +0700

    membuat DetailMataKuliahViewModel.kt

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.ucp2.ui.viewmodel.matakuliah
+
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.ui.viewmodel.dosen.DosenEvent
+
+data class DetailUiState(
+    val detailUiEvent: DosenEvent = DosenEvent (),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == DosenEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != DosenEvent ()
+}
+
+fun Dosen.toDetailUiEvent () : DosenEvent {
+    return DosenEvent(
+        nidn = nidn,
+        nama = nama,
+        jenisKelamin = jenisKelamin
+    )
+}
